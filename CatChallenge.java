59076d4ffe0be66edc7add3b03b6a4e9474fa1ad
nya10148 <98441661+nya10148@users.noreply.github.com>
2022-04-26 09:08:40 -0400

    Add files via upload

---
+++ CatChallenge.java
@@ -0,0 +1,13 @@
+
+/******************************************************************************* * * Kean University
+* Spring 2022
+* Course: CPS*2232 – Data Structures
+* Author: A'nya Carr, 2232*1
+* Homework : 1, task 1, 2, 3, ....
+* Program Description: *************************************************************************/
+
+package cat_challenge;
+
+public class CatChallenge {
+
+}
